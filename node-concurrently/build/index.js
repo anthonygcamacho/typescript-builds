c0a0cfeab255456e6d930b5cb1f5797256476a24
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-01-16 16:20:46 -0800

    building

--- node-concurrently/build/index.js
+++ node-concurrently/build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('HW2');
+Object.defineProperty(exports, "__esModule", { value: true });
+// import { Sorter } from "./Sorter"
+const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const numbersCollection = new NumbersCollection_1.NumberCollections([10, 30, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection("TxopWeV");
+charactersCollection.sort();
+console.log(charactersCollection.data);
--- node-concurrently/src/index.ts
+++ node-concurrently/src/index.ts
@@ -1,1 +1,11 @@
-console.log('HW2')
+// import { Sorter } from "./Sorter"
+import { NumberCollections } from "./NumbersCollection"
+import { CharactersCollection } from "./CharactersCollection"
+
+const numbersCollection = new NumberCollections([10, 30, -5, 0])
+numbersCollection.sort()
+console.log(numbersCollection.data)
+
+const charactersCollection = new CharactersCollection("TxopWeV")
+charactersCollection.sort()
+console.log(charactersCollection.data)
