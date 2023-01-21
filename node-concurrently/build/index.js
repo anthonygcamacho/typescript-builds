02f7935b75a72d066b12e76c4833f4e4164e02a3
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-01-20 18:51:59 -0800

    buillding

--- node-concurrently/build/index.js
+++ node-concurrently/build/index.js
@@ -1,11 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { Sorter } from "./Sorter"
-const NumbersCollection_1 = require("./NumbersCollection");
-const CharactersCollection_1 = require("./CharactersCollection");
-const numbersCollection = new NumbersCollection_1.NumberCollections([10, 30, -5, 0]);
-numbersCollection.sort();
-console.log(numbersCollection.data);
-const charactersCollection = new CharactersCollection_1.CharactersCollection("TxopWeV");
-charactersCollection.sort();
-console.log(charactersCollection.data);
+const MatchReader_1 = require("./csvreader/composition/MatchReader");
+const CsvFileReader_1 = require("./csvreader/composition/CsvFileReader");
+const ConsoleReport_1 = require("./csvreader/reportTargets/ConsoleReport");
+const WinsAnalyzer_1 = require("./csvreader/analyzers/WinsAnalyzer");
+const Summary_1 = require("./csvreader/Summary");
+// Inheretance
+// const reader = new MatchReader("football.csv")
+// reader.read()
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
+const summary = new Summary_1.Summary(new WinsAnalyzer_1.WinsAnalysis("Man United"), new ConsoleReport_1.ConsoleReport());
+// const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport())
+summary.buildAndPrintReport(matchReader.matches);
+// let manUnitedWins = 0
+// // Inheretance
+// // for (let match of reader.data) {
+// // Composition
+// for (let match of matchReader.matches) {
+//     if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//         manUnitedWins++
+//     } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//         manUnitedWins++
+//     }
+// }
+// console.log(`Man United won ${manUnitedWins} games`)
+// ------------------------------------------------------------------------------
+// SORTER
+// import { NumberCollections } from "./sorter/NumbersCollection"
+// import { CharactersCollection } from "./sorter/CharactersCollection"
+// const numbersCollection = new NumberCollections([10, 30, -5, 0])
+// numbersCollection.sort()
+// console.log(numbersCollection.data)
+// const charactersCollection = new CharactersCollection("TxopWeV")
+// charactersCollection.sort()
+// console.log(charactersCollection.data)
--- node-concurrently/src/index.ts
+++ node-concurrently/src/index.ts
@@ -1,11 +1,47 @@
-// import { Sorter } from "./Sorter"
-import { NumberCollections } from "./NumbersCollection"
-import { CharactersCollection } from "./CharactersCollection"
+import { MatchReader } from "./csvreader/composition/MatchReader"
+import { CsvFileReader } from "./csvreader/composition/CsvFileReader"
+import { ConsoleReport } from "./csvreader/reportTargets/ConsoleReport"
+import { WinsAnalysis } from "./csvreader/analyzers/WinsAnalyzer"
+import { Summary } from "./csvreader/Summary"
+import { HtmlReport } from "./csvreader/reportTargets/HtmlReport"
 
-const numbersCollection = new NumberCollections([10, 30, -5, 0])
-numbersCollection.sort()
-console.log(numbersCollection.data)
+// Inheretance
+// const reader = new MatchReader("football.csv")
+// reader.read()
 
-const charactersCollection = new CharactersCollection("TxopWeV")
-charactersCollection.sort()
-console.log(charactersCollection.data)
+const csvFileReader = new CsvFileReader("football.csv")
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
+
+const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport())
+// const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport())
+
+summary.buildAndPrintReport(matchReader.matches)
+
+// let manUnitedWins = 0
+// // Inheretance
+// // for (let match of reader.data) {
+// // Composition
+// for (let match of matchReader.matches) {
+//     if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//         manUnitedWins++
+//     } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//         manUnitedWins++
+//     }
+// }
+
+// console.log(`Man United won ${manUnitedWins} games`)
+
+// ------------------------------------------------------------------------------
+
+// SORTER
+// import { NumberCollections } from "./sorter/NumbersCollection"
+// import { CharactersCollection } from "./sorter/CharactersCollection"
+
+// const numbersCollection = new NumberCollections([10, 30, -5, 0])
+// numbersCollection.sort()
+// console.log(numbersCollection.data)
+
+// const charactersCollection = new CharactersCollection("TxopWeV")
+// charactersCollection.sort()
+// console.log(charactersCollection.data)
