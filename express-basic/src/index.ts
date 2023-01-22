fb7d1ed3fa3de988adeb0194aae75f14f91144c9
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-01-21 17:19:13 -0800

    building

---
+++ express-basic/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express"
+import { router } from "./routes/loginRoutes"
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(router)
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
