2df6e8317a83eebda17250d1543f0aa580be891a
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-01-24 21:17:42 -0800

    building

--- nestjs-01/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Module, Get } from "@nestjs/common"
-import { AppController } from "./app.controller"
-
-@Module({
-    controllers: [AppController],
-})
-export class AppModule {}
